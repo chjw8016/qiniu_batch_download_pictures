d4d37987bf5cd99975958819d1b36552dee65829
chjw <chjw@gammainfo.com>
2018-04-13 17:12:03 +0800

    init

---
+++ DowloadApp/Db.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace DowloadApp
+{
+    class DB
+    {
+        /// <summary>
+        /// 获取农场信息
+        /// </summary>
+        /// <returns>List</returns>
+        static public IList<DataItem> GetFarmData()
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            MySqlConnection myConnnect = new MySqlConnection(config.AppSettings.Settings["connectionString"].Value);
+            myConnnect.Open();
+            String sql = "select id,name from farm_info";
+            IList<DataItem> list = new List<DataItem>();
+            using (MySqlCommand myCmd = new MySqlCommand(sql, myConnnect))
+            {
+                MySqlDataReader dr = myCmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    if (dr.HasRows)
+                    {
+                        list.Add(new DataItem() { Id = Convert.ToInt32(dr[0]), Name = Convert.ToString(dr[1]) });
+                    }
+                }
+                dr.Close();
+            }
+            myConnnect.Close();
+            return list;
+        }
+        /// <summary>
+        /// 获取地块信息
+        /// </summary>
+        /// <param name="farmId"></param>
+        /// <returns>List</returns>
+        static public IList<DataItem> GetPlanData(Int32 farmId)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            MySqlConnection myConnnect = new MySqlConnection(config.AppSettings.Settings["connectionString"].Value);
+            myConnnect.Open();
+            String sql = "select id,code from farm_plan";
+            if (farmId > 0)
+            {
+                sql += " where farm_id=" + farmId;
+            }
+            IList<DataItem> list = new List<DataItem>();
+            using (MySqlCommand myCmd = new MySqlCommand(sql, myConnnect))
+            {
+                MySqlDataReader dr = myCmd.ExecuteReader();
+                list.Add(new DataItem() { Id = 0, Name = "全部地块" });
+                while (dr.Read())
+                {
+                    if (dr.HasRows)
+                    {
+                        list.Add(new DataItem() { Id = Convert.ToInt32(dr[0]), Name = Convert.ToString(dr[1]) });
+                    }
+                }
+                dr.Close();
+            }
+            myConnnect.Close();
+            return list;
+        }
+        /// <summary>
+        /// 获取作物信息
+        /// </summary>
+        /// <param name="planId"></param>
+        /// <param name="year"></param>
+        /// <returns>List</returns>
+        static public IList<DataItem> GetCaseData(int planId, String year)
+        {
+            if (year == "")
+            {
+                year = Convert.ToString(DateTime.Now.Year);
+            }
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)); // 当地时区
+            long end_year = (long)(Convert.ToDateTime(year + "-01-01 00:00:00").AddYears(1) - startTime).TotalSeconds;
+            long start_year = (long)(Convert.ToDateTime(year + "-01-01 00:00:00") - startTime).TotalSeconds;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            MySqlConnection myConnnect = new MySqlConnection(config.AppSettings.Settings["connectionString"].Value);
+            myConnnect.Open();
+            String sql = "select id,variety,sub_species,batch_date from farm_plan_case where create_time>=" + start_year + " and create_time<" + end_year;
+            if (planId > 0)
+            {
+                sql += " and farm_plan_id=" + planId;
+            }
+            IList<DataItem> list = new List<DataItem>();
+            using (MySqlCommand myCmd = new MySqlCommand(sql, myConnnect))
+            {
+                MySqlDataReader dr = myCmd.ExecuteReader();
+                list.Add(new DataItem() { Id = 0, Name = "全部作物" });
+                while (dr.Read())
+                {
+                    if (dr.HasRows)
+                    {
+                        list.Add(new DataItem() { Id = Convert.ToInt32(dr[0]), Name = Convert.ToString(dr[1]) + "/" + Convert.ToString(dr[2]) + "/" + Convert.ToString(dr[3]) });
+                    }
+                }
+                dr.Close();
+            }
+            myConnnect.Close();
+            return list;
+        }
+        /// <summary>
+        /// 获取年份
+        /// </summary>
+        /// <returns>List</returns>
+        static public IList<DataItem> GetYearData()
+        {
+            IList<DataItem> list = new List<DataItem>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(new DataItem() { Id = 0, Name = Convert.ToString(DateTime.Now.AddYears(0 - i).Year) });
+            }
+            return list;
+        }
+        /// <summary>
+        /// 获取农场图片
+        /// </summary>
+        /// <param name="caseId"></param>
+        /// <returns>DataTable</returns>
+        static public DataTable getPics(int farmId, int planId, int caseId, String year)
+        {
+            if (year == "")
+            {
+                year = Convert.ToString(DateTime.Now.Year);
+            }
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)); // 当地时区
+            long end_year = (long)(Convert.ToDateTime(year + "-01-01 00:00:00").AddYears(1) - startTime).TotalSeconds;
+            long start_year = (long)(Convert.ToDateTime(year + "-01-01 00:00:00") - startTime).TotalSeconds;
+            DataTable dt = new DataTable();
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            MySqlConnection myConnnect = new MySqlConnection(config.AppSettings.Settings["connectionString"].Value);
+            myConnnect.Open();
+
+            String sql = "select c.create_time,f.code as land, c.id,c.pic,c.variety,c.sub_species,c.batch_date from case_state_pests c left join farm_plan f on c.farm_plan_id =f.id where c.farm_id=" + farmId + " and c.create_time>=" + start_year + " and c.create_time<" + end_year;
+            if (planId > 0)
+            {
+                sql += " and c.farm_plan_id" + planId;
+            }
+            if (caseId > 0)
+            {
+                sql += " and c.case_id" + caseId;
+            }
+            using (MySqlCommand myCmd = new MySqlCommand(sql, myConnnect))
+            {
+                MySqlDataReader dr = myCmd.ExecuteReader();
+                dt.Load(dr);
+                dr.Close();
+            }
+            myConnnect.Close();
+            return dt;
+        }
+
+    }
+
+    class DataItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class ThreadData
+    {
+        public string Url { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ DowloadApp/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Qiniu.Storage;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.Threading;
+using System.IO;
+
+namespace DowloadApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BindFarm();
+            BindYear();
+        }
+
+        public void BindFarm()
+        {
+            cbb_farm.ItemsSource = DB.GetFarmData();
+            cbb_farm.DisplayMemberPath = "Name";
+            cbb_farm.SelectedValuePath = "Id";
+            cbb_farm.SelectedIndex = 0;
+        }
+        public void BindCase(int planId)
+        {
+            cbb_case.ItemsSource = DB.GetCaseData(planId, Convert.ToString(cbb_year.Text));
+            cbb_case.DisplayMemberPath = "Name";
+            cbb_case.SelectedValuePath = "Id";
+            cbb_case.SelectedIndex = 0;
+        }
+        public void BindPlan(int farmId)
+        {
+            cbb_plan.ItemsSource = DB.GetPlanData(farmId);
+            cbb_plan.DisplayMemberPath = "Name";
+            cbb_plan.SelectedValuePath = "Id";
+            cbb_plan.SelectedIndex = 0;
+        }
+        public void BindYear()
+        {
+            cbb_year.ItemsSource = DB.GetYearData();
+            cbb_year.DisplayMemberPath = "Name";
+            cbb_year.SelectedValuePath = "Id";
+            cbb_year.SelectedIndex = 0;
+        }
+        private void CbbFarm_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BindPlan(Convert.ToInt32(cbb_farm.SelectedValue));
+        }
+        private void CbbPlan_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BindCase(Convert.ToInt32(cbb_plan.SelectedValue));
+        }
+
+        private void CbbCase_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState != WindowState.Normal)
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void btn_download_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable list = DB.getPics(Convert.ToInt32(cbb_farm.SelectedValue), Convert.ToInt32(cbb_plan.SelectedValue), Convert.ToInt32(cbb_case.SelectedValue), cbb_year.Text);
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            String url = config.AppSettings.Settings["qiniu_url"].Value;
+
+            IList<ThreadData> arr = new List<ThreadData>();
+            for (int i = 0; i < list.Rows.Count; i++)
+            {
+                JArray array = (JArray)JsonConvert.DeserializeObject(Convert.ToString(list.Rows[i]["pic"]));
+                foreach (var jObject in array)
+                {
+                    String fileName = cbb_year.Text + "_" + list.Rows[i]["land"] + "_" + list.Rows[i]["variety"] + "_" + list.Rows[i]["sub_species"] + "_" + list.Rows[i]["batch_date"] + "=" + list.Rows[i]["create_time"] + "." + jObject["ext"];
+                    //DownloadManager.Download(url + Convert.ToString(jObject["filename"]), fileName);
+                    url += Convert.ToString(jObject["filename"]);
+                    arr.Add(new ThreadData() { Url = url, FileName = fileName });
+                    //ConsoleBox.AppendText("开始下载：" + fileName + "\n");
+                }
+            }
+            ConsoleBox.AppendText("[ " + DateTime.Now.ToString() + " ] 开始下载，总计" + arr.Count + "个文件\n");
+            ThreadPool.QueueUserWorkItem(DownloadPic, arr);
+            //ConsoleBox.AppendText("下载完成：" + DateTime.Now.ToLocalTime().ToString() + "\n");
+        }
+        public void DownloadPic(object obj)
+        {
+            List<ThreadData> list = (List<ThreadData>)obj;
+            foreach (ThreadData item in list)
+            {
+                String path = Environment.CurrentDirectory + "\\" + item.FileName.Replace("_", "\\").Split('=')[0];
+                //ConsoleBox.Dispatcher.Invoke(new Action(() => { ConsoleBox.AppendText("下载：" + item.FileName); }));
+                if (File.Exists(path + "\\" + item.FileName) == false)
+                {
+                    DownloadManager.Download(item.Url, item.FileName);
+                    ConsoleBox.Dispatcher.Invoke(new Action(() =>
+                    {
+                        ConsoleBox.AppendText("[ " + DateTime.Now.ToString() + " ] " + item.FileName + "，下载完成！\n");
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        File.Move(item.FileName, path + "\\" + item.FileName);
+                    }));
+                }
+                else
+                {
+                    ConsoleBox.Dispatcher.Invoke(new Action(() =>
+                    {
+                        ConsoleBox.AppendText("[ " + DateTime.Now.ToString() + " ] " + item.FileName + "，已下载完成！\n");
+                    }));
+                }
+
+            }
+        }
+    }
+
+}
